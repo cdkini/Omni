09a3731ee1aaae2e2e6e7caf6a0d6c031c082a44
Chetan Kini <ckini123@gmail.com>
2020-09-07 19:01:34 -0400

    Moved CLI args out of Main and to Command.java; added test files for both

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,66 +1,6 @@
 public class Main {
-
-    public static void init() {
-        // TODO: FILL IN
-    }
-
-    public static void add(String filename) {
-        // TODO: FILL IN
-    }
-
-    public static void commit(String msg) {
-        // TODO: FILL IN
-    }
-
-    public static void rm(String filename) {
-        // TODO: FILL IN
-    }
-
-    public static void log() {
-        // TODO: FILL IN
-    }
-
-    public static void globalLog() {
-        // TODO: FILL IN
-    }
-
-    public static void find(String msg) {
-        // TODO: FILL IN
-    }
-
-    public static void status() {
-        // TODO: FILL IN
-    }
-
-    public static void checkOutFile(String fileName) {
-        // TODO: FILL IN
-    }
-
-    public static void checkOutCommit(String commmitID, String filename) {
-        // TODO: FILL IN
-    }
-
-    public static void checkOutBranch(String branchName) {
-        // TODO: FILL IN
-    }
-
-    public static void branch(String branchName) {
-        // TODO: FILL IN
-    }
-
-    public static void rmBranch(String branchName) {
-        // TODO: FILL IN
-    }
-
-    public static void reset(String commitID) {
-        // TODO: FILL IN
-    }
-
-    public static void merge(String branchName) {
-        // TODO: FILL IN
-    }
-
     public static void main(String[] args) {
         // TODO: FILL IN
     }
 }
+
