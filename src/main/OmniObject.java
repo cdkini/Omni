9c523884249d098c8b8b2b4a5378713126e1ffb3
Chetan Kini <ckini123@gmail.com>
2020-09-24 16:54:55 -0400

    Added serialize and deserialize wrappers around Utils to OmniObject ABC

--- src/main/OmniObject.java
+++ src/main/OmniObject.java
@@ -1,11 +1,21 @@
 package src.main;
 
+import java.io.File;
 import java.io.Serializable;
 
 public abstract class OmniObject implements Serializable {
-    protected String hash;
+    private String ID;
+    private static File DIR = new File(".omni");
 
-    protected void setHash(Object... vals) {
-        this.hash = Utils.sha1(vals);
+    public void setID(Object... vals) {
+        this.ID = Utils.sha1(vals);
+    }
+
+    public void serialize() {
+        Utils.serialize(this, ID, new File(".omni"));
+    }
+
+    public static OmniObject deserialize(String fileName) {
+        return (OmniObject) Utils.deserialize(fileName, DIR);
     }
 }
