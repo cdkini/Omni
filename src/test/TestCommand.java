e20fd171181084bf692c8720b1cd0fe0d43c1fe9
Chetan Kini <ckini123@gmail.com>
2020-09-24 11:54:50 -0400

    Updated test fixtures and added file content test

--- src/test/TestCommand.java
+++ src/test/TestCommand.java
@@ -1,30 +1,53 @@
 package src.test;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
 import org.junit.rules.TemporaryFolder;
 import src.main.Command;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static org.junit.Assert.assertTrue;
+
 public class TestCommand {
+    private String mockPath;
 
     @Rule
     public TemporaryFolder mockDir = new TemporaryFolder();
 
-    @Test
+    @Before
+    public void setPath() {
+        mockPath = mockDir.getRoot().getAbsolutePath();
+    }
+
+    @After
+    public void resetPath() {
+        mockPath = "";
+    }
+
+    @Test (expected = Exception.class)
     public void InitWithExistingDirectoryShouldFail() throws IOException {
-        // TODO: Open to implement test!
-        String mockPath = mockDir.getRoot().getAbsolutePath();
+        Files.createDirectory(Paths.get(mockPath+".omni/"));
         Command.init(mockPath);
     }
 
     @Test
     public void InitShouldCreateDirectoryAndSubdirectories() throws IOException {
-        // TODO: Open to implement test!
-        String mockPath = mockDir.getRoot().getAbsolutePath();
         Command.init(mockPath);
+        assertTrue(Files.isDirectory(Paths.get(mockPath+".omni/objects/")));
+        assertTrue(Files.isDirectory(Paths.get(mockPath+".omni/branches/")));
+        assertTrue(Files.isDirectory(Paths.get(mockPath+".omni/refs/heads")));
+        assertTrue(Files.exists(Paths.get(mockPath + ".omni/HEAD")));
+    }
+
+    @Test
+    public void InitShouldSetProperFileContents() throws IOException {
+        // TODO: Open to implement test!
+        Command.init(mockPath);
+        assertTrue(false);
     }
 }
