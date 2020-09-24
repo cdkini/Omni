2961ed725cdf1d90de3c9531320efd06739b5280
Chetan Kini <ckini123@gmail.com>
2020-09-24 16:29:58 -0400

    Set up basic structure of OmniObject and Blob

--- src/main/Blob.java
+++ src/main/Blob.java
@@ -1,1 +1,14 @@
+package src.main;
 
+import java.io.File;
+import java.io.Serializable;
+
+public class Blob extends OmniObject implements Serializable {
+    private String fileName;
+    private byte[] contents;
+
+    public Blob(File file) {
+        this.contents = Utils.readContents(file);
+        this.fileName = file.getName();
+    }
+}
--- src/main/OmniObject.java
+++ src/main/OmniObject.java
@@ -1,1 +1,11 @@
+package src.main;
 
+import java.io.Serializable;
+
+public abstract class OmniObject implements Serializable {
+    protected String hash;
+
+    protected void setHash(Object... vals) {
+        this.hash = Utils.sha1(vals);
+    }
+}
